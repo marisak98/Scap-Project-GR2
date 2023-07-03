3475d902595606b6482140bf54556ad4d7dc59d5
marisak <sanchezJordan.sh@gmail.com>
2023-07-03 17:48:38 -0500

    login test panel add

--- src/main/java/com/app/Scap/view/menuScap.java
+++ src/main/java/com/app/Scap/view/menuScap.java
@@ -1,14 +1,13 @@
 package com.app.Scap.view;
 
-import com.app.Scap.ScapApplication;
+
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplicationShutdownHandlers;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -20,33 +19,49 @@ import javafx.scene.text.Text;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
-import java.io.IOException;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 public class menuScap  extends Application {
     @FXML
     private Button myButton;
     @FXML
+    private Label lblMensaje;
+
+    @FXML
+    private ImageView imageView;
+
+
+    public void imageLogo(URL url, ResourceBundle rb) {
+        File file = new File("img/icon.png");
+        Image image = new Image(file.toURI().toString());
+        imageView.setImage(image);
+    }
+    @FXML
     private void handleButtonClick(ActionEvent event)  {
         System.out.printf("¡WTF Soy un Boton!!");
+        lblMensaje.setText("WTF soy un boton!!");
 
     }
     @Override
     public void start(Stage primaryStage) throws Exception{
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/static/test.fxml"));
-        StackPane stackPane = loader.load();
+        GridPane stackPane = loader.load();
         Scene scene = new Scene( new Group());
         scene.getStylesheets().add(getClass().getResource("/static/style.css").toExternalForm());
 
-        stackPane.getStyleClass().add("my-rect");
-
-        Text text = new Text("Hola culeros");
-        text.getStyleClass().add("test");
-
-
-        stackPane.getChildren().add(text);
+        stackPane.getStyleClass().add("body");
         scene.setRoot(stackPane);
+
+        lblMensaje = (Label) loader.getNamespace().get("lblMensaje");
+
+        
 
         myButton = (Button) loader.getNamespace().get("myButton");
         AnchorPane.setTopAnchor(myButton, 10.0);
